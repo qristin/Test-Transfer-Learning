60796b3c9259a8816fd92be7039491cb8fce69ba
Kristin Rieping <kristin@q42.nl>
2017-12-03 21:20:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Transfer learning with Tensorflow
+Training the final layer of the google inception model with given input images and predict which class an image belongs to.
+
+## Prequisits
+
+To be able to train and use the model you need to install the following:
+- python 3 installed
+- tensorflow installed `pip install tensorflow`
+
+Create a folder named data. See 'data-example' for the expected structure.
+
+## Train the model
+
+To train the last layer of the network run the following code
+```
+python retrain.py --image_dir <path to current dir>/data/training-data/images/ --output-graph <path to current dir>/data/training-data/models/output-graph.pb --output-labels <path to current dir>/data/training-data/models/output_labels.txt --bottleneck_dir ./data/training-data/models/bottleneck --how_many_training_steps 100
+```
+
+### Predict
+
+```
+python predict.py --test-image ./data/test/vase.png --labels ./data/training-data/models/output_labels.txt --graph ./data/training-data/models/output_graph.pb
+```
+
+###Help
+
+Depending on the OS the path sometimes is not correctly found. The tensorflow lib then writes the models in your os tmp folder.
---
+++ predict.py
@@ -0,0 +1,75 @@
+import argparse
+import sys
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+import tensorflow as tf
+
+def load_image(filename):
+  """Read in the image_data to be classified."""
+  return tf.gfile.FastGFile(filename, 'rb').read()
+
+
+def load_labels(filename):
+  """Read in labels, one label per line."""
+  return [line.rstrip() for line in tf.gfile.GFile(filename)]
+
+
+def load_graph(filename):
+  """Unpersists graph from file as default graph."""
+  with tf.gfile.FastGFile(filename, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    tf.import_graph_def(graph_def, name='')
+
+
+def run_graph(image_data, labels, input_layer_name, output_layer_name,
+              num_top_predictions):
+  with tf.Session() as sess:
+    # Feed the image_data as input to the graph.
+    #   predictions will contain a two-dimensional array, where one
+    #   dimension represents the input image count, and the other has
+    #   predictions per class
+    softmax_tensor = sess.graph.get_tensor_by_name(output_layer_name)
+    predictions, = sess.run(softmax_tensor, {input_layer_name: image_data})
+
+    # Sort to show labels in order of confidence
+    top_k = predictions.argsort()[-num_top_predictions:][::-1]
+    for node_id in top_k:
+      human_string = labels[node_id]
+      score = predictions[node_id]
+      print('%s (score = %.5f)' % (human_string, score))
+
+    return top_k
+
+def predict(labels, graph, image):
+  pil_im = Image.open(image, 'r')
+  plt.figure()
+  plt.imshow(np.asarray(pil_im))
+  
+  # load image
+  image_data = load_image(image)
+  
+  # load labels
+  labels = load_labels(labels)
+  
+  # load graph, which is stored in the default session
+  load_graph(graph)
+
+  return run_graph(image_data, labels, 'DecodeJpeg/contents:0', 'final_result:0', 5)
+
+parser = argparse.ArgumentParser(description='Predict if images belongs to class bowl or vase')
+
+parser.add_argument('--test-image', help='Path to image that needs to be tested')
+parser.add_argument('--graph', help='Path to the model graph')
+parser.add_argument('--labels', help='Path to labels')
+
+def main():
+  args = parser.parse_args()
+  predict(args.labels, args.graph, args.test_image)
+
+if __name__ == '__main__':
+    main()
+
+# results = predict('./inception-2015-12-05/imagenet_synset_to_human_label_map.txt', './inception-2015-12-05/classify_image_graph_def.pb', '/media/kristin/DATA/Projects/Borges/Test/Borges_assessment_data/datadir/vase/png/0f2a22f0-c67d-42b9-9e87-43e09e48fa1f.png')
